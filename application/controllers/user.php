b5caa89f3092e16028857ac13d59910ccf399883
unknown <selin.dha12@gmail.com>
2018-05-06 21:18:13 +0700

    pesan

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -1,13 +1,28 @@
 <?php
 
-class user extends CI_Controller
-{
-	public function form()
-	{
-		$this->load->view("form_v");
+class user extends CI_Controller{
+	//function auto run
+	function __construct(){
+		parent::__construct();
+		//load model
+		$this->load->model("user_m")
 	}
-	public function add()
-	{
-		echo $this->input->post("username");
-	}	
+	public function form(){
+		$this->load->view("form_v")
+	}
+	
+	public function add(){
+		//create data array
+		$data = array(
+			"username" => $this->input->post("username"),
+			"password" => $this->input->post("password"),
+			"fullname" => $this->input->post("fullname"),
+			"level" => $this->input->post("level") 
+		);
+
+		//check data before save
+		var_dump($data);
+		//save data to database
+		$this->user_m->add($data);
+	}
 }
--- application/views/form_v.php
+++ application/views/form_v.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<meta charset="utf-8" />
+	<title>Form Input - User</title>
 </head>
 <body>
-	<form action:"http://localhost/codeigniter/index.php/user/add method="POST">
-		Username :<input name="username" value=""><br/>
-		Pass : <input name="password" type="password" value=""><br/>
-		Fullname :<input name="fullname" value=""><br/>
-		Level :<input name="level" value=""><br/>
-		<input name="tombol" type="submit" value="simpan">
+
+	<form action:"http://localhost/travelseli/index.php/user/add method="POST">
+		<tr><td>Username<br/><input name="username" value=""></td></tr>
+		<tr><td>Password<br/><input name="password" type="password" value=""></td></tr>
+		<tr><td>Fullname<br/><input name="fullname" value=""></td></tr>
+		Level<br/>
+		<select name="level">
+			<option value="admin">Admin</option>
+			<option value="user">User</option>
+		<select><br/>
+		<input type="submit" value="simpan" />
 	</form>
 </body>
 </html>
